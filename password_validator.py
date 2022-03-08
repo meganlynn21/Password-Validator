6b90118aadebf6b1d23b0ce0624a6ccdfd94c7c6
meganlynn21 <meganlkeyes0203@gmail.com>
2022-03-07 20:28:08 -0500

    I changed the formatting around so the code will run faster. Checked for lowercase letters Checked for numbers Checked for special characters Checked to make sure password does not contain initials

--- password_validator.py
+++ password_validator.py
@@ -1,42 +1,67 @@
 # Password Validator
 
-#Function that gets inputted name's initials
+#Gets inputted name's initials
 
-
-def get_initials():
-    name = input("What is your first and last name?: ")
-    initials = ""
-    for char in name:
-    #check uppercase characters
-        if char.isupper():
-            initials += char
-    return print(initials)
+name = input("What is your first and last name?: ")
+initials = ""
+for char in name:
+#check uppercase characters
+    if char.isupper():
+        initials += char
+print(initials)
 
 #Function that checks and validates the password
-def check_password():
-    password = input("What is your desired password? ")
-    get_four_char = password[0:4].lower()
-    length_password = len(password)
-    i = True
-    while i:
-        if length_password < 8 or length_password > 12:
-            print("Password must be between 8 and 12 characters")
-            check_password()
-        if get_four_char == "pass":
-            print("Password cannot start with Pass")
-            check_password()
-        else:
-            i = False
-        for char in password:
-            if char.isupper():
-                print("IT WORKS")
-                break
-            else:
-                print("Password must contain at least 1 uppercase letter")
-                break
+def main(min_len: int, max_len: int):
+    special_char = "!@#$%^"
+    #Check password input and return password string.
+    while True:
+        password = input("What is your desired password? ")
+        
+        # -------------------- Check for Bad Inputs -------------------- #
+        # Check for input.
+        if not password:
+            print("Please enter a password.\n")
+            continue
+        
+        # Check if initials are in password
+        elif initials.lower() in password or initials.upper() in password:
+            print("Cannot have initials in password")
 
-get_initials()
-check_password()
+        # Check for weak password.
+        elif "pass" in password or "Pass" in password:
+            print("Password cannot start with pass.\n")
+            continue
+
+        # Check for length.
+        elif min_len < len(password) < max_len:
+            print(f"Password must be between {min_len} and {max_len} characters.\n")
+            continue
+
+        # Check for Uppercase letters
+        elif not any(letter.isupper() for letter in password):
+            print("Password must contain at least 1 uppercase letter.\n")
+            continue
+
+        # Check for lowercase letters
+        elif not any(letter.islower() for letter in password):
+            print("Password must contain at least 1 lowercase letter.\n")
+            continue
+
+        # Check if there is at least one number between 0 and 9
+        elif not any(letter.isdigit() for letter in password):
+            print("Password must have a number between 0 and 9")
+
+        # Check for special characters
+        elif not any(letter in special_char for letter in password):
+            print("Password must contain a special character")
+
+        # -------------------- End of Bad Inputs -------------------- #
+        
+        else:
+            return password
+
+
+main(8,12)
 
 
 

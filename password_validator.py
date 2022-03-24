667de7e87d3fe1fc7a1d6e2d980f3c9da1c126c0
meganlynn21 <meganlkeyes0203@gmail.com>
2022-03-23 22:19:48 -0400

    Changed the lists to dictionaries

--- password_validator.py
+++ password_validator.py
@@ -1,5 +1,5 @@
 def main():
-  lstDuplicates = []
+  dictionary = {}
   isValid = True
   hasUpper = False
   hasLower = False
@@ -11,6 +11,7 @@ def main():
   sInitials = sName[0] + sName[sName.find(" ")+1]
   # # Ask for password
   sPassword = input("What is your desired password? ")
+    
 # ----------- CHECKING FOR ERRORS -----------------------
 
   # Check password length
@@ -25,52 +26,65 @@ def main():
 
   # Iterate over the password
   for char in sPassword:
+      
     # Check for uppercase letters
     if char.isupper():
-      hasUpper = True  
+      hasUpper = True
+        
     # Check for lowercase letters   
     if char.islower():
       hasLower = True
+        
     # Check for numbers
     if char.isdigit():
       hasDigit = True
+        
     # Check for special characters
     if char in "!@#$%^":  
-      hasSpecialChar = True
+      hasSpecialChar = True    
+        
   # Check to see if character appears more than once
-    sPassword = sPassword.lower() 
-    if sPassword.count(char) > 1:
-      hasDuplicate = True
-      if char not in lstDuplicates:
-        lstDuplicates.append(char)
-          
+  for char in sPassword.lower():
+       sPassword = sPassword.lower()
+       if sPassword.count(char) > 1:
+           hasDuplicate = True
+           dictionary[char] = dictionary.get(char,0) + 1
+       else:
+        dictionary[char] = 0 
+        
   # Print if uppercase char needed
   if not hasUpper:
     print("Password must contain at least 1 uppercase letter.")
     isValid = False
+      
   # Print if lower case char needed
   if not hasLower:
     print("Password must contain at least 1 lowercase letter.")
     isValid = False
+      
   # Print if no number present
   if not hasDigit:
     print("Password must have a number between 0 and 9")
     isValid = False
+      
   # Print if no special char
   if not hasSpecialChar:
     print("Password must contain at least 1 of these special characters: ! @ # $ % ^")
     isValid = False
+      
   # Check for initials
   if sInitials.lower() in sPassword:
     print("Password must not contain user initials")
     isValid = False 
+      
   # Print Duplicate Chars
   if hasDuplicate: 
     print("These characters appear more than once: ")
     isValid = False
-    for entry in lstDuplicates:
-      print(entry + ": " + str(sPassword.count(entry)) + " times")
-
+    for entry, count in dictionary.items():
+        if count > 1:
+          print(entry,": ",count," times")
+            
 # #------------- END OF ERROR CHECKING ---------------------- 
   if isValid:
     print("Password is valid and OK to use.")
